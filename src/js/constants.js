a7df7ddca053dd855697c798111fdb4980ec4cfe
yassinphilip <xaccrocheur@gmail.com>
2025-11-30 21:46:22 +0100

    SRC Cleanup

--- src/js/constants.js
+++
@@ -1,5 +0,0 @@
-// Platform information - imports from single source of truth
-import { PLATFORMS, getPlatformInfo as getPlatformInfoFromSource } from './platforms.js';
-
-// Re-export for convenience
-export { PLATFORMS, getPlatformInfoFromSource as getPlatformInfo };

c7b2545bdb1d234b885d00703d813b92b2fd5f58
yassinphilip <xaccrocheur@gmail.com>
2026-04-15 09:39:33 +0100

    New Logo

--- src/js/backends.js
+++ src/js/backends.js
@@ -5,7 +5,6 @@ import { fetchImages as opdbFetch } from './backends/opdb.js';
 import { fetchGameMetaData } from './backends/wikipedia-text.js';
 
 export const getGameMetaData = async (params) => {
-    console.log("params: ", params);
     return await fetchGameMetaData(params.cleanName, params.platformName);
 };
 
@@ -14,8 +13,6 @@ export const getAllCoverImageUrls = async (gameName, platform, platformName, opt
 
     const backends = [];
 
-    // For VPX platform, prioritize OPDB results first
-    console.log("platform: ", platform);
     if (platform === 'vpx') {
         if (opdbAPIKey) {
             backends.push(() => opdbFetch(gameName, opdbAPIKey));
@@ -28,7 +25,6 @@ export const getAllCoverImageUrls = async (gameName, platform, platformName, opt
         }
         backends.push(() => wikipediaFetch(gameName, platformName));
     } else {
-        // Default order for other platforms
         if (steamGridAPIKey) {
             backends.push(() => steamgridFetch(gameName, steamGridAPIKey));
         }
